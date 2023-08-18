8d5b4a9a49c4edaafe996b6875ccbcf8e3a14a78
Pavlo Netesa <pnetesa@gmail.com>
2023-08-18 11:07:01 +0300

    Completed: 'codewars-style-ranking-system'

--- src/codewars-style-ranking-system.js
+++ src/codewars-style-ranking-system.js
@@ -16,12 +16,13 @@ const { strictEqual, expect } = require('./util/test');
 // throw any progress away). The exception is if there is no other rank left to progress towards (Once you reach rank 8
 // there is no more progression).
 // - A user cannot progress beyond rank 8.
-// - The only acceptable range of rank values is -8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8. Any other value should raise an error.
+// - The only acceptable range of rank values is -8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8. Any other value should raise
+// an error.
 //
 // The progress is scored like so:
 // - Completing an activity that is ranked the same as that of the user's will be worth 3 points
 // - Completing an activity that is ranked one ranking lower than the user's will be worth 1 point
-// - Any activities completed that are ranking 2 levels or more lower than the user's ranking will be ignored
+// - Any activities completed that are ranking 2 levels or lower than the user's ranking will be ignored
 // - Completing an activity ranked higher than the current user's rank will accelerate the rank progression. The greater
 // the difference between rankings the more the progression will be increased. The formula is
 //                   10 * d * d
@@ -31,7 +32,8 @@ const { strictEqual, expect } = require('./util/test');
 // - If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
 // - If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
 // - If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
-// - If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being upgraded to rank -7 and having earned 60 progress towards their next rank
+// - If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being
+// upgraded to rank -7 and having earned 60 progress towards their next rank
 // - If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)
 //
 // Code Usage Examples:
@@ -45,7 +47,8 @@ const { strictEqual, expect } = require('./util/test');
 // user.progress # => 0 // progress is now zero
 // user.rank # => -7 // rank was upgraded to -7
 //
-// Note: Codewars no longer uses this algorithm for its own ranking system. It uses a pure Math based solution that gives consistent results no matter what order a set of ranked activities are completed at.
+// Note: Codewars no longer uses this algorithm for its own ranking system. It uses a pure Math based solution that
+// gives consistent results no matter what order a set of ranked activities are completed at.
 
 class User {
   progress;
@@ -56,45 +59,33 @@ class User {
     this.progress = progress;
   }
 
-  // The formula is
-  //    10 * d * d
-  // where d equals the difference in ranking between the activity and the user.
   incProgress(rank) {
     if (rank < -8 || rank > 8 || rank === 0) {
       throw new Error('Invalid rank value. Allowed range is -8 to 8 excluding 0');
     }
 
-    const diff = (Math.sign(rank) !== Math.sign(this.rank)) ? rank - this.rank - 1 : rank - this.rank;
-    if (diff >= 1) {
+    if (this.rank === 8) {
+      this.progress = 0;
+      return;
+    }
+
+    const diff = (Math.sign(rank) !== Math.sign(this.rank)) ? Math.abs(rank - this.rank) - 1 : Math.abs(rank - this.rank);
+    if (diff >= 1 && rank > this.rank) {
       this.progress += 10 * diff * diff;
     } else if (diff === 0) {
       this.progress += 3;
-    } else if (diff === -1) {
+    } else if (diff === 1) {
       this.progress += 1;
-    } else if (diff < -1) {
-      // no progress increase
     }
 
     if (this.progress > 100 && this.rank < 8) {
       do {
-        this.rank = this.rank + 1;
-        this.progress = this.progress - 100;
+        this.rank = (this.rank + 1) || this.rank + 2;
+        this.progress = (this.rank < 8) ? this.progress - 100 : 0;
       } while (this.progress > 100);
     }
   }
 }
-
-// let user = new User();
-// user.incProgress(-4);
-// console.log('rank: ', user.rank);
-// console.log('progress: ', user.progress);
-//
-// user = new User(-1);
-// user.incProgress(1);
-// console.log('rank: ', user.rank);
-// console.log('progress: ', user.progress);
-//
-// console.log('------------------------------------------------------------------');
 
 let user = new User();
 
