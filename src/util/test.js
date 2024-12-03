093d6c69ee3a004c0914af277ee053a12509b80a
Pavlo Netesa <pnetesa@gmail.com>
2024-12-03 09:00:02 +0200

    Use deepEqualImpl()

--- src/util/test.js
+++ src/util/test.js
@@ -1,8 +1,35 @@
 const util = require('util');
 const assert = require('assert');
+
+function deepEqualImpl(value, other) {
+  if (typeof value !== typeof other) {
+    return false;
+  }
+
+  if (Number.isNaN(value) && Number.isNaN(other)) {
+    return true;
+  }
+
+  if (typeof value !== 'object' || value === null || other === null) {
+    return value === other;
+  }
+
+  if (Object.keys(value).length !== Object.keys(other).length) {
+    return false;
+  }
+
+  for (const key of Object.keys(value)) {
+    if (!deepEqualImpl(value[key], other[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function strictEqual(actual, expected, message = '') {
   try {
-    assert(actual === expected, message);
+    assert(deepEqualImpl(actual, expected), message);
     console.log(message ? `${message} - PASS` : `-> ${expected} - PASS`);
   } catch (e) {
     console.log(`EXPECTED: ${expected} ACTUAL: ${actual} ${message} - FAIL`);
@@ -11,7 +38,7 @@ function strictEqual(actual, expected, message = '') {
 
 function deepEqual(actual, expected, message = '') {
   try {
-    assert(util.inspect(actual, {}) === util.inspect(expected), message);
+    assert(deepEqualImpl(actual, expected), message);
     console.log(message ? `${util.inspect(message)} - PASS` : `-> ${util.inspect(expected)} - PASS`);
   } catch (e) {
     console.log(`EXPECTED: ${util.inspect(expected)} ACTUAL: ${util.inspect(actual)} ${message} - FAIL`);
@@ -26,7 +53,6 @@ function expect(actual, message = '') {
     console.log(`ACTUAL: ${actual} ${message} - FAIL`);
   }
 };
-
 
 module.exports.strictEqual = strictEqual;
 module.exports.deepEqual = deepEqual;
